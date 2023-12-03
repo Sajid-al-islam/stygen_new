0716a4b05a948766fc95e28f1e1eb3ccf1afba6d
sajidalislam <sajid285.me@gmail.com>
2023-12-03 19:11:00 +0600

    fixing issues

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        '/pay-via-ajax', '/success','/cancel','/fail','/ipn','/fbapi', 'createpayment', 'executepayment'
+        '/pay-via-ajax', '/success','/cancel','/fail','/ipn','/fbapi', 'createpayment', 'executepayment', 'livewire/*'
     ];
 }
